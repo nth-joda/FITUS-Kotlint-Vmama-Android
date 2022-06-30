a1a89bdc765e1f71a25f759b923a459c7ee3190d
khietvbonus <nltkhiet18@apcs.vn>
2022-06-30 11:37:22 +0700

    voucher srceen

---
+++ app/src/main/java/com/fitus/vscannerandroid/VoucherScreen.java
@@ -0,0 +1,14 @@
+package com.fitus.vscannerandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoucherScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voucher_screen);
+    }
+}
