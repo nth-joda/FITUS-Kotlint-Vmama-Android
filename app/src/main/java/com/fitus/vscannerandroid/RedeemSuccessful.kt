daaf78a77d756bb9511e41293dd237b489c06a02
khietvbonus <nltkhiet18@apcs.vn>
2022-07-26 10:18:01 +0700

    redeem failed & successful

---
+++ app/src/main/java/com/fitus/vscannerandroid/RedeemSuccessful.kt
@@ -0,0 +1,11 @@
+package com.fitus.vscannerandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RedeemSuccessful : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_redeem_successful)
+    }
+}
